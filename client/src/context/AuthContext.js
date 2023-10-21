02fbca46f0a9d36f8ee6ad7ad80cf4d404864c08
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-10-21 10:53:41 +0700

    null localStorage

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,7 +2,7 @@ import { createContext, useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-  user:JSON.parse(localStorage.getItem("user")) || null,
+  user:JSON.parse(localStorage.getItem("user")) || null, // Mendapatkan data pengguna dari localStorage jika ada, atau set null jika tidak ada
   isFetching: false,
   error: false,
 };
